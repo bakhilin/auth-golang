e21649e32ecd52064090181edc8b71b043b68721
Bakhilin Nikita <bahilinnikita04@mail.ru>
2024-05-09 15:09:07 +0300

    authentication added

--- controller/main.go
+++ controller/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"log"
 	server "v1"
+	"v1/lib/handler"
 )
 
 func main(){
+	handlers := new(handler.Handler)		
 	server := new(server.Server)
-	error := server.Run("8000")
+	error := server.Run("8000", handlers.InitRoutes())
 	if error != nil {
 		log.Fatal("error running http server")
 	}
--- server.go
+++ server.go
@@ -9,9 +9,10 @@ type Server struct {
 	httpServer *http.Server
 }
 
-func (s *Server) Run(port string) error {
+func (s *Server) Run(port string, handler http.Handler) error {
 	s.httpServer = &http.Server{
 		Addr: ":" + port,
+		Handler: handler,
 	}
 
 	return s.httpServer.ListenAndServe()
